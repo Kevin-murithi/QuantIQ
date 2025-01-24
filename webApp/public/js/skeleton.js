38744c93d20e543fe7390f3d27463d7732e0d0a9
Denzel <denzelk741@gmail.com>
2025-01-24 21:28:45 +0300

    Skeleton almost done...

---
+++ webApp/public/js/skeleton.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector('.toggler');
+
+toggler.addEventListener('click', () => {
+  document.querySelector('.skeleton').classList.toggle('active');
+  document.querySelector('.parent_container').classList.toggle('respond');
+})
