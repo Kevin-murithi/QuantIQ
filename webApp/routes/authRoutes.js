f5830ef558cd4c17d95940bfdc3cabeb9c7841aa
kevin-murithi <murithikevin54@gmail.com>
2025-01-24 12:18:41 +0300

    custom fields

--- webApp/routes/authRoutes.js
+++ webApp/routes/authRoutes.js
@@ -11,12 +11,9 @@ var jsonParser = bodyParser.json();
 // Page rendering routes
 router.get ("/", loadPages.landingPage);
 router.get ("/signup", loadPages.signupPage);
-<<<<<<< HEAD
 router.get ("/login", loadPages.loginPage);
 router.get("/customfield", loadPages.customfieldspage);
-=======
 router.get ("/signin", loadPages.loginPage);
->>>>>>> 85ab000ca1e50d3c9ae010d136895ff9f8a0fd44
 
 // Other (CRUD/auth) routes
 
