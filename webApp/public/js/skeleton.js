11504c47d23c5936175d2af3d03ed6b84bdb0cfb
Denzel <denzelk741@gmail.com>
2025-01-24 21:51:52 +0300

    ...

--- webApp/public/js/skeleton.js
+++ webApp/public/js/skeleton.js
@@ -2,5 +2,4 @@ const toggler = document.querySelector('.toggler');
 
 toggler.addEventListener('click', () => {
   document.querySelector('.skeleton').classList.toggle('active');
-  //document.querySelector('.parent_container').classList.toggle('respond');
 })
