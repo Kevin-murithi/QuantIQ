4b308aa0366a075fced56ef2081e3d2c18b7f601
Denzel <denzelk741@gmail.com>
2025-01-25 09:20:19 +0300

    Template created...

--- webApp/routes/authRoutes.js
+++ webApp/routes/authRoutes.js
@@ -11,11 +11,20 @@ var jsonParser = bodyParser.json();
 
 // Page rendering routes
 router.get ("/", loadPages.landingPage);
-router.get ("/dashboard", loadPages.dashboard);
 router.get ("/signup", loadPages.signupPage);
 router.get ("/login", loadPages.loginPage);
-router.get("/customfield", loadPages.customfieldspage);
 router.get ("/signin", loadPages.loginPage);
+router.get("/customfield", loadPages.customfieldspage);
+router.get ("/dashboard", loadPages.dashboard);
+router.get ("/ai_insights", loadPages.ai_insights);
+router.get ("/customer_insights", loadPages.customer_insights);
+router.get ("/forecasting", loadPages.forecasting);
+router.get ("/esg_metrics", loadPages.esg_metrics);
+router.get ("/sentiment_analysis", loadPages.sentiment_analysis);
+router.get ("/account_management", loadPages.account_management);
+router.get ("/files", loadPages.files);
+router.get ("/settings", loadPages.settings);
+router.get ("/sales_analytics", loadPages.sales_analytics);
 
 // Other (CRUD/auth) routes
 
